03739eae10e2f91270128881fa0932fe3d79e2b5
unknown <dummynoikofi@gmail.com>
2020-03-01 19:43:10 +0300

    Created App Container

---
+++ djbackend/assets/reactjs/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import './App.css';
+import Home from "./components/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello from React</h1>
+    </div>
+  );
+}
+
+export default App;
