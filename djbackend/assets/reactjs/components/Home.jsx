e8d5fe4d63e65e3a6222cead9e72ee045974b96d
unknown <dummynoikofi@gmail.com>
2020-03-02 11:35:04 +0300

    Created Home component

---
+++ djbackend/assets/reactjs/components/Home.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Home extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Home component</h2>
+            </div>
+        )
+    }
+}
