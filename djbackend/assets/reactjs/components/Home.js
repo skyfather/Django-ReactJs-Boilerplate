49deb70f021f9393e582372f10ca22c6b068d0cd
unknown <dummynoikofi@gmail.com>
2020-03-03 10:16:15 +0300

    renamed to .js

---
+++ djbackend/assets/reactjs/components/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Home extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Home component</h2>
+                <h4>Changes there</h4>
+            </div>
+        )
+    }
+}
